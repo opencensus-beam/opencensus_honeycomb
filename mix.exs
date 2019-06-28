8c25ad4e787c622c1fbd5020e1f20d49ca99cdd2
Garth Kidd <garth@amplified.ai>
2019-06-28 13:23:35 +1000

    Publish 0.2.0.

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Opencensus.Honeycomb.MixProject do
       ],
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
-      version: "0.1.2"
+      version: "0.2.0"
     ]
   end
 
